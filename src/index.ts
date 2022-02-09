9ed6a81d900de8f3f55864888adb70bee5990d6a
Aubin <aolivrie77@gmail.com>
2022-02-09 11:00:22 +0100

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,9 @@
-import { questions } from './questions' 
-
 export function randomTabItem(arr) {
     const selectedQuestions = [];
 
     for (let i = 0; i < 30; i++) {
         const number = Math.round(Math.random() * arr.length - 1);
-        selectedQuestions.push(questions[number]);
+        selectedQuestions.push(arr[number]);
     }
     
     return selectedQuestions;
